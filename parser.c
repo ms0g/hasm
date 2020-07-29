95b91ae936a75669dc050c8b7bf52adf783452d9
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2020-07-29 14:35:55 +0300

    fixed code repetition

--- parser.c
+++ parser.c
@@ -16,32 +16,37 @@ static u16 LC;
 /* RAM[16] */
 static u16 RAM_ADDR = 0x0010;
 
+static char buff[BUFF];
+static char token[100];
+static struct Symbol *sym;
+
+#define FLUSH() { \
+    memset(buff, 0, BUFF);\
+    memset(token, 0, 100);\
+}
 
 /* pass 1 */
 void init_analysis(FILE *infp, char *file_name, struct Symbol **sym_table) {
-    char buff[BUFF], token[100];
-    struct Symbol *sym;
     FILE *outfp;
     int tok_type;
-
-    memset(buff,0, BUFF);
-    memset(token, 0, 100);
-
+    
+    FLUSH()
+    
     outfp = hasm_fopen(file_name, "w");
-
+    
     while (fgets(buff, sizeof(buff), infp) != NULL) {
         sscanf(buff, "%s", buff);
-
+        
         if (tokenize(buff, token, &tok_type, NULL, pass1) < 0)
             continue;
-
+        
         switch (tok_type) {
             case LABEL:
                 insert_symtab(sym_table, token, LC);
                 break;
             case A_INS:
                 sym = scan_symtab(*sym_table, token);
-                if (!sym) {
+                if (sym == NULL) {
                     insert_symtab(sym_table, token, RAM_ADDR++);
                 }
                 break;
@@ -49,38 +54,35 @@ void init_analysis(FILE *infp, char *file_name, struct Symbol **sym_table) {
             default:
                 break;
         }
-
+        
         if (tok_type != LABEL) {
             fprintf(outfp, "%s\n", buff);
             // increase ROM address
             ++LC;
         }
-
+        
         // flush buffer
-        memset(token, 0, 100);
-        memset(buff,0, BUFF);
+        FLUSH()
     }
     hasm_fclose(outfp);
+    sym = NULL;
 }
 
 /* pass 2 */
 void init_synthesis(FILE *infp, FILE *outfp, struct Symbol **sym_table) {
-    char buff[BUFF], token[100];
     unsigned short dest, comp, jmp;
-    struct Symbol *sym;
     int tok_type;
-
-    memset(buff,0, BUFF);
-    memset(token,0, sizeof(token));
-
+    
+    FLUSH()
+    
     while (fgets(buff, sizeof(buff), infp) != NULL) {
         C_INS_t _inst;
         memset(&_inst, 0, sizeof(C_INS_t));
-
+        
         sscanf(buff, "%s", buff);
-
+        
         tokenize(buff, token, &tok_type, &_inst, pass2);
-
+        
         switch (tok_type) {
             case A_INS:
                 sym = scan_symtab(*sym_table, token);
@@ -103,17 +105,16 @@ void init_synthesis(FILE *infp, FILE *outfp, struct Symbol **sym_table) {
             default:
                 break;
         }
-
+        
         // keep byte order.Must be big-endian
         byte_buff = read_msb(byte_buff);
         hasm_fwrite(&byte_buff, sizeof(byte_buff), 1, outfp);
-
+        
         // flush buff
         byte_buff &= ~byte_buff;
-        memset(token, 0, sizeof(token));
-        memset(buff, 0, BUFF);
-
-
+        FLUSH()
+        
+        
         Hasm_Free(_inst.comp)
         Hasm_Free(_inst.dest)
         Hasm_Free(_inst.jmp)
